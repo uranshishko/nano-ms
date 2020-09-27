2c36b108f548a9deb1f3958117e8f26190eba23a
Uran Shishko <uranshishko@Urans-MacBook-Pro-2.local>
2020-09-27 15:42:03 +0200

    added documentation

--- src/utils/cookie-parser.js
+++ src/utils/cookie-parser.js
@@ -1,16 +1,22 @@
+/**
+ *
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+
 module.exports = async (req, res) => {
-	req.cookies = {};
+    req.cookies = {};
 
-	let cookies = req.getHeader('cookie');
+    let cookies = req.getHeader('cookie');
 
-	if (!cookies) {
-		return;
-	}
+    if (!cookies) {
+        return;
+    }
 
-	cookies = cookies.split(';');
+    cookies = cookies.split(';');
 
-	for (cookie of cookies) {
-		cookie = cookie.trim().split('=');
-		req.cookies[cookie[0]] = cookie[1];
-	}
+    for (cookie of cookies) {
+        cookie = cookie.trim().split('=');
+        req.cookies[cookie[0]] = cookie[1];
+    }
 };
