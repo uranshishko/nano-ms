27c378addb87180bb99827e0326272323dfc50c3
Uran Shishko <uranshishko@Urans-MacBook-Pro-2.local>
2020-09-27 15:42:27 +0200

    refactored code and fixed bugs

--- src/utils/request-handler.js
+++ src/utils/request-handler.js
@@ -1,113 +1,135 @@
+/**
+ * @typedef {(req: http.IncomingMessage, res: http.ServerResponse) => http.ServerResponse | void} HandlerFunction
+ * @typedef {{path: string, method: string, middleware: {[key: string]: HandlerFunction}, func: HandlerFunction}} Service
+ */
+
 const url = require('url');
 
 const cookieParser = require('./cookie-parser');
+const { NotFoundException } = require('../errors');
 
+/**
+ *
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {Service[][]} services
+ * @param {HandlerFunction[]} middleware
+ */
 async function requestHandler(req, res, services, middleware) {
-	const method = req.method;
-	const URL = url.parse(req.url);
+    const method = req.method;
+    const URL = url.parse(req.url);
+    let modifiedURL;
 
-	cookieParser(req, res);
+    //* Parse cookies
+    cookieParser(req, res);
 
-	let body = [];
+    let body = [];
 
-	//* Initialize url params object on http.IncomingMessage
-	req.params = {};
+    //* Initialize url params object on http.IncomingMessage
+    req.params = {};
 
-	//* If data is sent along with request, NanoMS stores it as a Buffer in req.body
-	//! NOTE: body-parsing middleware is required in order to read the data from req.body
-	//! Use built in JSON or url-encoded parsers (NanoMS.json OR NanoMS.urlEncoded)
-	req.on('data', (chunk) => body.push(chunk));
+    //* If data is sent along with request, NanoMS stores it as a Buffer in req.body
+    req.on('data', (chunk) => body.push(chunk));
 
-	req.on('end', async () => {
-		body = Buffer.concat(body);
-		req.body = body;
+    req.on('end', async () => {
+        body = Buffer.concat(body);
+        req.body = body;
 
-		//* Running globally defined middleware functions
-		for await (mw of middleware) {
-			try {
-				await mw(req, res);
+        //* Running globally defined middleware functions
+        for await (mw of middleware) {
+            try {
+                await mw(req, res);
 
-				const hasEnded = res.writableEnded === undefined ? res.finished : res.writableEnded;
-				if (hasEnded) {
-					return;
-				}
-			} catch (e) {
-				//* catch eventual errors and exeptions
-				errorHandler(e);
-			}
-		}
+                const hasEnded = res.writableEnded === undefined ? res.finished : res.writableEnded;
+                if (hasEnded) {
+                    return;
+                }
+            } catch (e) {
+                //* catch eventual errors and exeptions
+                errorHandler(e);
+            }
+        }
 
-		//* When request is finished NanoMS loops through services and matches path and method
-		for (service of services) {
-			//* Check if a service path accepts url parameters
-			const paramsRegex = /:[a-zA-Z0-9].+/g;
+        //* Searching for exact route
+        let foundService = services[0].find((service) => service.path === URL.pathname && service.method === method);
 
-			let parsedServicePath;
+        //* Searching for named routes
+        if (!foundService) {
+            foundService = services[1].find(findNamedRoute);
+        }
 
-			if (service.path.match(paramsRegex)) {
-				parsedServicePath = service.path.split('/');
-				let url = URL.pathname.split('/');
+        //* If neither is found, return 404
+        if (!foundService) {
+            return res.status(404).send({ statusCode: 404, message: 'Not Found' });
+        } //* Else
 
-				if (url[url.length - 1] === '') {
-					url.pop();
-				}
+        //* Go through locally assigned middleware
+        for (const mw in foundService.middleware) {
+            try {
+                await foundService.middleware[mw](req, res);
 
-				for (let i = 0; i < parsedServicePath.length; i++) {
-					if (parsedServicePath[i].match(paramsRegex)) {
-						//* Populate params object on http.IncomingMessage
-						req.params[parsedServicePath[i].replace(':', '')] = url[i];
-						parsedServicePath[i] = url[i];
-					}
-				}
+                const hasEnded = res.writableEnded === undefined ? res.finished : res.writableEnded;
+                if (hasEnded) {
+                    return;
+                }
+            } catch (e) {
+                //* catch eventual errors and exeptions
+                errorHandler(e);
+            }
+        }
 
-				parsedServicePath = parsedServicePath.join('/');
-				URL.pathname = url.join('/');
-			}
+        //* If no response was sent through middleware, process continues to service controller
+        try {
+            return await foundService.func(req, res);
+        } catch (e) {
+            //* catch eventual errors and exeptions
+            errorHandler(e);
+        }
+    });
 
-			if ((parsedServicePath ? parsedServicePath : service.path) === URL.pathname) {
-				if (service.method === method) {
-					//* And then through locally assigned middleware
-					for (const mw in service.middleware) {
-						try {
-							await service.middleware[mw](req, res);
+    function errorHandler(e) {
+        if (e.type && e.type === 'HttpException') {
+            return res.status(e.statusCode).send({
+                message: e.message,
+                statusCode: e.statusCode,
+            });
+        } else {
+            console.log(e);
+        }
+    }
 
-							const hasEnded = res.writableEnded === undefined ? res.finished : res.writableEnded;
-							if (hasEnded) {
-								return;
-							}
-						} catch (e) {
-							//* catch eventual errors and exeptions
-							errorHandler(e);
-						}
-					}
+    function findNamedRoute(service) {
+        //* Check if a service path accepts url parameters
+        const paramsRegex = /:[a-zA-Z0-9].+/g;
 
-					//* If no response was sent through middleware, process continues
-					try {
-						return await service.func(req, res);
-					} catch (e) {
-						//* catch eventual errors and exeptions
-						errorHandler(e);
-					}
-				}
-			}
+        let parsedServicePath;
 
-			continue;
-		}
+        if (service.path.match(paramsRegex)) {
+            parsedServicePath = service.path.split('/');
+            let url = URL.pathname.split('/');
 
-		//* If path is not found OR method type doesn't match NanoMS responds with a 404 Not found
-		res.status(404).send({ statusCode: 404, message: 'Not Found' });
-	});
+            if (url[url.length - 1] === '') {
+                url.pop();
+            }
 
-	function errorHandler(e) {
-		if (e.type && e.type === 'HttpException') {
-			return res.status(e.statusCode).send({
-				message: e.message,
-				statusCode: e.statusCode,
-			});
-		} else {
-			console.log(e);
-		}
-	}
+            for (let i = 0; i < parsedServicePath.length; i++) {
+                if (parsedServicePath[i].match(paramsRegex)) {
+                    //* Populate params object on http.IncomingMessage
+                    req.params[parsedServicePath[i].replace(':', '')] = url[i];
+                    parsedServicePath[i] = url[i];
+                }
+            }
+
+            parsedServicePath = parsedServicePath.join('/');
+            modifiedURL = url.join('/');
+        }
+
+        if (!(parsedServicePath === modifiedURL && service.method === method)) {
+            req.params = {};
+        }
+
+        return parsedServicePath === modifiedURL && service.method === method;
+    }
 }
 
 module.exports = requestHandler;
